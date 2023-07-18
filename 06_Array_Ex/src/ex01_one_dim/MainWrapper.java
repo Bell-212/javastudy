030b630508b9addf7119f97dff9c943adddffc52
Bell-212 <helloj0212@gmail.com>
2023-07-18 12:45:19 +0900

    20230718

--- 06_Array_Ex/src/ex01_one_dim/MainWrapper.java
+++ 06_Array_Ex/src/ex01_one_dim/MainWrapper.java
@@ -1,17 +1,19 @@
 package ex01_one_dim;
 
+import java.util.Arrays;
+
 public class MainWrapper {
 
   public static void ex01() {
-    // 월에 따른 계절    월 % 12
-    // 3 ~ 5 : 봄        3 ~ 5
-    // 6 ~ 8 : 여름      6 ~ 8
-    // 9 ~ 11: 가을      9 ~ 11
-    // 12 ~ 2: 겨울      0 ~ 2
-    int month = 3;
+    // 월에 따른 계절    월 % 12    인덱스
+    // 3 ~ 5 : 봄        3 ~ 5      1 
+    // 6 ~ 8 : 여름      6 ~ 8      2
+    // 9 ~ 11: 가을      9 ~ 11     3
+    // 12 ~ 2: 겨울      0 ~ 2      0
+    int month = 11;
     String[] seasons = {"겨울", "봄", "여름", "가을"};
     // 코드 1줄로 해결할 것
-    
+    System.out.println( month + "월은 " + seasons[(month % 12) / 3] + "입니다." );
   }
   
   public static void ex02() {
@@ -19,7 +21,17 @@ public class MainWrapper {
     // 배열 a: [10, 20, 30, 40, 50]
     int[] a = new int[5];
     // 배열과 인덱스 외 변수 사용 금지
-    
+    /*인덱스 - 배열요소 -> 식
+    a[0] = 10;            (인덱스 + 1) * 10            
+    a[1] = 20;
+    a[2] = 30;
+    a[3] = 40;
+    a[4] = 50;
+    */
+    for(int i = 0; i < a.length; i++) {
+      a[i] = (i + 1) * 10;
+    }
+    System.out.println("배열 a: " + Arrays.toString(a));
   }
   
   public static void ex03() {
@@ -27,15 +39,26 @@ public class MainWrapper {
     // 배열 a: [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
     char[] a = new char[26];
     char ch = 'A';
-    
+    for(int i = 0; i < a.length; i++) {
+      a[i] = (char)(ch + i);
+    }
+    System.out.println("배열 a: " + Arrays.toString(a));
   }
+  
   
   public static void ex04() {
     // 배열 a의 마지막 3개 요소 0을 배열 b의 요소로 바꾸기
     // 배열 a: [10, 20, 30, 40, 50, 60]
     int[] a = {10, 20, 30, 0, 0, 0};
     int[] b = {40, 50, 60};
+    System.arraycopy(b, 0, a, 3, b.length);
+    System.out.println("배열 a: " + Arrays.toString(a));
     
+    /* for문으로 해결할 경우
+     * for(int i = 0; i < b.length; i++) { 
+     *  a[3 + i] = b[i]; 
+     * }
+     */
   }
   
   public static void ex05() {
@@ -45,8 +68,23 @@ public class MainWrapper {
                                  // ┌------------------------------------------------┐
     int[] binary = new int[10];  // │ 0 │ 0 │ 0 │ 0 │ 1 │ 0 │ 0 │ 0 │ 1 │ 1 │
                                  // └------------------------------------------------┘
-    
+    /*
+     * int i = 0; while(number > 0) { binary[i] = number % 2; number = number / 2;
+     * i++; } for(int j = binary.length - 1; j >= 0; j--) {
+     * System.out.print(binary[j]); }System.out.println();
+     */
+    System.out.println("10진수 " + number + " = 2진수 ");
+    int i = 9;
+    while(number != 0) {  // while(number > 0)
+      binary[i--] = number % 2;
+      number /= 2;    //number = number / 2;
+    }
+    for(int j = 0; j < binary.length; j++) {
+      System.out.print(binary[j]);
+    }
   }
+  
+  
   
   public static void ex06() {
     // 배열에 저장된 점수의 평균, 최댓값, 최솟값 구하기
@@ -64,10 +102,10 @@ public class MainWrapper {
   public static void main(String[] args) {
     
     //ex01();
-    ex02();
+    //ex02();
     //ex03();
     //ex04();
-    //ex05();
+    ex05();
     //ex06();
     
     
