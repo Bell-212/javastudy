cab69559b4a5c90e415d5b6c5905daeae680d3fa
Bell-212 <helloj0212@gmail.com>
2023-07-28 16:55:56 +0900

    20230728

---
+++ 12_CollectionFramework_Ex/src/ex06_BinaryNumber/MainWrapper.java
@@ -0,0 +1,12 @@
+package ex06_BinaryNumber;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+
+    BinaryNumber bn = new BinaryNumber(1024);
+    System.out.println(bn);  // 100000
+    
+  }
+
+}
